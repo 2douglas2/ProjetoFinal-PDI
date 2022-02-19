838d2fe9f3585421c4ddb5dad3554c57a7f0030a
Felype <36242789+felypemaciel@users.noreply.github.com>
2022-02-19 14:38:57 -0300

    Atualizacao preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,12 @@
 def preprocessing(img):
+    # transformacao gamma
+    lookUpTable = np.empty((1,256), np.uint8)
+    for i in range(256):
+        lookUpTable[0,i] = np.clip(pow(i / 255.0, .2) * 255.0, 0, 255)
+    # resultado da transformacao gamma
+    res_light = cv2.LUT(img, lookUpTable)
     # conversao da imagem para escala de cinza
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(res_light, cv2.COLOR_BGR2GRAY)
     # aplicacao de filtro gaussiano
     imgblur = cv2.GaussianBlur(img_gray, (3,3), 0)
     # aplicacao de filtro laplaciano
@@ -16,8 +22,5 @@ def preprocessing(img):
     # opening
     kernel = np.ones((13,13),np.uint8)
     opening = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
-    # dilatacao
-    kernel_dl = np.ones((3,3),np.uint8)
-    dilation = cv2.dilate(opening,kernel_dl,iterations = 1)
     
-    return dilation
+    return opening
